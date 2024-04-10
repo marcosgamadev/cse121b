6c03db57f4ff0704cf6fdb513c47c1e1605485fe
marcosgamadev <marcosgama.dev@gmail.com>
2024-04-10 17:48:01 -0300

    new style

--- final-project.js
+++ final-project.js
@@ -1,12 +1,12 @@
-let key = "3b7067cf7e933d736b0e75856853a827"
-
+let key = "3b7067cf7e933d736b0e75856853a827";
+let cities = [];
 
 function screen(data){
-    console.log(data)
-    document.querySelector(".city").innerHTML = "Weather in " + data.name
-    document.querySelector(".temp").innerHTML =  Math.floor(data.main.temp) + "°C"
-    document.querySelector(".description").innerHTML = data.weather[0].description
-    document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + data.weather[0].icon + ".png"
+    console.log(data);
+    document.querySelector(".city").innerHTML = "Weather in " + data.name;
+    document.querySelector(".temp").innerHTML =  Math.floor(data.main.temp) + "°C";
+    document.querySelector(".description").innerHTML = data.weather[0].description;
+    document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + data.weather[0].icon + ".png";
 }
 
 async function search(city){
@@ -17,14 +17,28 @@ async function search(city){
     "&lang=en" +
     "&units=metric"
     )
-    .then(answer => answer.json())
+    .then(answer => answer.json());
 
-    screen(data)
+    screen(data);
 }
-
 
 function clickTheBottom(){
-   let city = document.querySelector(".input-city").value
-
-   search(city)
+   let city = document.querySelector(".input-city").value;
+   cities.push(city); 
+   search(city);
+   displayCities(); 
 }
+
+function displayCities() {
+    let cityListElement = document.querySelector(".city-list");
+ 
+    cityListElement.innerHTML = "";
+
+    
+    cities.forEach(function(city) {
+        let listItem = document.createElement("li");
+        listItem.textContent = city;
+        cityListElement.appendChild(listItem);
+    });
+}
+
